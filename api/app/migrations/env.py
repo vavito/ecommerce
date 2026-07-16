e973cfc6264645485f154b304ef80fbe5ca957ab
vavito <joaovictor2126@gmail.com>
2026-07-16 10:26:37 -0300

    config: arquivo models em /app carrega todos os models

--- api/app/migrations/env.py
+++ api/app/migrations/env.py
@@ -7,7 +7,7 @@ from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from app.core.config import settings
-from app.shared.base_model import BaseModel
+from app.models import BaseModel
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
---
+++ api/app/models.py
@@ -0,0 +1,4 @@
+from app.modules.user.models import Address, User
+from app.shared.base_model import BaseModel
+
+__all__ = ["Address", "BaseModel", "User"]
